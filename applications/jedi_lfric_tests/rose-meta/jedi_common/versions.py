0b1ff09181ff2be0891d861e34e54efc75eca110
James Manners <james.manners@metoffice.gov.uk>
2026-01-20 10:36:04 +0000

    #135: Ran $UMDIR/SimSys_Scripts/lfric_macros/apply_macros.py vn3.0_t135

--- applications/jedi_lfric_tests/rose-meta/jedi_common/versions.py
+++ applications/jedi_lfric_tests/rose-meta/jedi_common/versions.py
@@ -31,3 +31,16 @@ class vnXX_txxx(MacroUpgrade):
         # Add settings
         return config, self.reports
 """
+
+
+class vn30_t135(MacroUpgrade):
+    """Upgrade macro for ticket #135 by James Manners."""
+
+    BEFORE_TAG = "vn3.0"
+    AFTER_TAG = "vn3.0_t135"
+
+    def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/socrates-radiation
+        self.add_setting(config, ["namelist:cosp", "n_cosp_step"], "1")
+
+        return config, self.reports
--- interfaces/socrates_interface/rose-meta/socrates-radiation/versions.py
+++ interfaces/socrates_interface/rose-meta/socrates-radiation/versions.py
@@ -20,13 +20,10 @@ class UpgradeError(Exception):
 
 """
 Copy this template and complete to add your macro
-
 class vnXX_txxx(MacroUpgrade):
     # Upgrade macro for <TICKET> by <Author>
-
     BEFORE_TAG = "vnX.X"
     AFTER_TAG = "vnX.X_txxx"
-
     def upgrade(self, config, meta_config=None):
         # Add settings
         return config, self.reports
@@ -34,11 +31,13 @@ class vnXX_txxx(MacroUpgrade):
 
 
 class vn30_t135(MacroUpgrade):
-    # Upgrade macro for #135 by James Manners
+    """Upgrade macro for ticket #135 by James Manners."""
 
     BEFORE_TAG = "vn3.0"
     AFTER_TAG = "vn3.0_t135"
 
     def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/socrates-radiation
         self.add_setting(config, ["namelist:cosp", "n_cosp_step"], "1")
+
         return config, self.reports
